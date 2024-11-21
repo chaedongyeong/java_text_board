d4c0b00f37f58497c0ab173ca9d59887e6a7b3f5
chaedongyeong <ydc4739@naver.com>
2024-11-21 23:06:43 +0900

    게시물 작성시 작성된 정보를 Article 객체에 저장

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 자바 텍스트 게시판 시작 ==");
+        int lastArticleId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = sc.nextLine();
@@ -17,9 +19,15 @@ public class Main {
                 System.out.print("내용 : ");
                 String content = sc.nextLine();
 
-                int id = 1;
+                int id = ++lastArticleId;
 
-                System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+                Article article = new Article();
+                article.id = id;
+                article.subject = subject;
+                article.content = content;
+
+                System.out.println("생성된 게시물 객체 : " + article);
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
             }
             else if (cmd.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
@@ -34,4 +42,10 @@ public class Main {
         System.out.println("== 자바 텍스트 게시판 종료 == ");
         sc.close();
     }
+}
+
+class Article {
+    int id;
+    String subject;
+    String content;
 }
