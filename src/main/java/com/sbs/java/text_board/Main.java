060f6d1d6c50e33a6f52ebaf5c940512687c23c2
chaedongyeong <ydc4739@naver.com>
2024-11-22 16:08:47 +0900

    게시물 리스트에서 출력을 최신순으로

--- src/main/java/com/sbs/java/text_board/Main.java
+++ src/main/java/com/sbs/java/text_board/Main.java
@@ -49,14 +49,14 @@ public class Main {
 
                 System.out.println("== 게시물 리스트 ==");
                 System.out.println("번호 | 제목");
-//                for (int i = 0; i < articles.size(); i++) {
-//                    Article article = articles.get(i);
-//                    System.out.printf("%d | %s\n", article.id, article.subject);
-//                }
+                for (int i = articles.size() - 1; i >= 0; i--) {
+                    Article article = articles.get(i);
+                    System.out.printf("%d | %s\n", article.id, article.subject);
+                }
 
-                articles.forEach(
-                  article -> System.out.printf("%d | %s\n", article.id, article.subject)
-                );
+//                articles.forEach(
+//                  article -> System.out.printf("%d | %s\n", article.id, article.subject)
+//                );
             }
             else if (cmd.equals("/usr/article/detail")) {
                 Article article = lastArticle;
